4100f1e5c8c9ee6a24c4b3c7b2906a045c8accd5
soungsid <soungsid@gmail.com>
2022-10-29 10:46:38 -0400

    Project initialization

---
+++ src/main/kotlin/com/sms/amazonproduct/amazonproduct/AmazonProductApplication.kt
@@ -0,0 +1,11 @@
+package com.sms.amazonproduct.amazonproduct
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AmazonProductApplication
+
+fun main(args: Array<String>) {
+    runApplication<AmazonProductApplication>(*args)
+}
---
+++ src/test/kotlin/com/sms/amazonproduct/amazonproduct/AmazonProductApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sms.amazonproduct.amazonproduct
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class AmazonProductApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
