d6965ad447e3e08d132d3cf7246dab0e5e35c45e
soungsid <soungsid@gmail.com>
2022-10-29 14:10:39 -0400

    Switch from kotlin to java

---
+++ src/test/java/com/sms/amazonproduct/AmazonProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sms.amazonproduct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmazonProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
